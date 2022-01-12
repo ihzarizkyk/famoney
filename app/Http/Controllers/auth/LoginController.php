2688faa64b272f4e33a51c09c846f47f7162929f
ihzarizkyk <ihzarizky30@gmail.com>
2022-01-12 12:38:05 +0700

    commits

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -4,21 +4,46 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class LoginController extends Controller
 {
+
+    public function __construct()
+    {
+        return $this->middleware(["guest"])->except("logout");
+    }
+
     public function login()
     {
-        //
+        return view("auth.login");
     }
 
-    public function postlogin()
+    public function postlogin(Request $req)
     {
-        //
+        $req->validate([
+            "name" => "required",
+            "password" => "required"]);
+
+        if(Auth::attempt(["name" => $req->name,"password" => $req->password]))
+        {
+            $req->session()->regenerate();
+            return redirect("dashboard.index");
+        }else{
+            Flash::message("login gagal!");
+            return redirect()->back();
+        }
+
     }
 
-    public function logout()
+    public function logout(Request $req)
     {
-        //
+        Auth::logout();
+
+        $req->session()->invalidate();
+        $req->session()->regenerateToken();
+
+        return redirect("auth.login");
     }
 }
--- app/Http/Controllers/auth/RegisterController.php
+++ app/Http/Controllers/auth/RegisterController.php
@@ -4,16 +4,36 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     public function register()
     {
-        //
+        return view("auth.register");
     }
 
-    public function postregister()
+    public function postregister(Request $req)
     {
-        //
+        $req->validate([
+            "name" => "required",
+            "email" => "required",
+            "password" => "required"]);
+
+        if(Auth::check())
+        {
+            return redirect()->back();
+        }else{
+            $usr = User::create([
+                "name" => $req->name,
+                "emai" => $req->email,
+                "password" => Hash::make($req->password)]);
+            $req->session()->regenerate();
+
+            Auth::login($usr);
+            
+            return redirect("dashboard.index");
+        }
     }
 }
