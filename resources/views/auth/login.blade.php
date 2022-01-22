05ad67d24354d2fe9fcb4744bfedd5c0d56472db
ihzarizkyk <ihzarizky30@gmail.com>
2022-01-22 18:43:32 +0700

    update view login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,12 +2,12 @@
 @section('title','Login - Famoney App')
 @section('content')
 	<div class="container">
-		<div class="row d-flex justify-content-center mt-3">
-			<div class="col col-md-6">
+		<div class="row d-flex justify-content-center mt-3 px-5 py-5">
+			<div class="col col-lg-6">
 				<div class="card">
 					<div class="card-body">
-						<h3 class="card-title">
-							Login Form - Famoney App
+						<h3 class="card-title fw-bolder">
+							Login - Famoney App
 						</h3>
 						<form action="{{route('post.login')}}" class="form" method="POST" name="loginform">
 							@csrf
@@ -20,14 +20,37 @@
 								<label for="password"><b>Password</b></label>
 								<input type="password" class="form-control" name="password" autocomplete="off">
 							</div>
+							<p class="text-secondary mt-2"><i class="fas fa-question-circle"></i> Don't have an account? <a href="/auth/register">Register Here</a></p>
 							<br>
-							<input type="submit" class="btn btn-primary rounded-pill" value="Login">
-							<br><br>
-							<p class="text-secondary">Don't have an account? <a href="/auth/register">Register Here</a></p>
+							<div class="text-center">
+								<input type="submit" class="btn-lg btn-primary rounded-pill" value="Login">
+							</div>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
+
+
+		<div class="text-center mt-1 mb-5 px-5 py-5">
+			<p class="text-white" id="footer">
+				&copy; 2022	by Famoney App Team
+			</p>
+		</div>
+
 	</div>
 @endsection
+@once
+	@push('css')
+	<style>
+	body{
+		background: #2980b9;  /* fallback for old browsers */
+		background: -webkit-linear-gradient(to right, #2c3e50, #2980b9);  /* Chrome 10-25, Safari 5.1-6 */
+		background: linear-gradient(to right, #2c3e50, #2980b9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+		}
+	.card{
+		border-radius: 10px;
+	}
+	</style>
+	@endpush
+@endonce
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,12 +2,12 @@
 @section('title','Register - Famoney App')
 @section('content')
 	<div class="container">
-		<div class="row d-flex justify-content-center mt-3">
-			<div class="col col-md-6">
+		<div class="row d-flex justify-content-center mt-3 px-5 py-5">
+			<div class="col col-lg-6">
 				<div class="card">
 					<div class="card-body">
-						<h3 class="card-title">
-							Register Form - Famoney App
+						<h3 class="card-title fw-bolder">
+							Register - Famoney App
 						</h3>
 						<form action="{{route('post.register')}}" class="form" method="POST" name="registerform">
 							@csrf
@@ -38,13 +38,41 @@
 								@enderror
 							</div>
 							<br>
-							<input type="submit" class="btn btn-primary rounded-pill" value="Register">
-							<br><br>
-							<p class="text-secondary">Already Have an Account? <a href="/auth/login">Please login here</a> </p>
+							<p class="text-secondary mt-2"><i class="fas fa-question-circle"></i> Already Have an Account? <a href="/auth/login">Please login here</a> </p>
+							<br>
+							<div class="text-center">
+								<input type="submit" class="btn -lg btn-primary rounded-pill" value="Register">
+							</div>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
+
+		<div class="text-center mt-1 mb-5 px-5 py-5">
+			<p class="text-white" id="footer">
+				&copy; 2022	by Famoney App Team
+			</p>
+		</div>
+
 	</div>
 @endsection
+@once
+	@push('css')
+	<style>
+	body{
+		background: #2980b9;  /* fallback for old browsers */
+		background: -webkit-linear-gradient(to right, #2c3e50, #2980b9);  /* Chrome 10-25, Safari 5.1-6 */
+		background: linear-gradient(to right, #2c3e50, #2980b9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+		}
+
+	.card{
+		border-radius: 10px;
+	}
+
+/*	input['text']{
+		border-radius: 10px;
+	}*/
+	</style>
+	@endpush
+@endonce
